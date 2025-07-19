14b1608d3cc78457e5e03439b20d63f8e539b5a5
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-20 00:21:16 +0530

    test

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,51 +1,19 @@
-resource "aws_iam_role" "lambda_exec_role" {
+resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role-${var.environment}"
 
   assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Action    = "sts:AssumeRole"
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
       }
-    ]
+    }]
   })
 
   tags = {
     Environment = var.environment
+    Name        = "lambda-execution-role"
   }
-}
-
-resource "aws_iam_role_policy" "lambda_ecr_policy" {
-  name = "lambda-ecr-access"
-  role = aws_iam_role.lambda_exec_role.id
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:DescribeImages"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "ecr_read_only" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
--- terraform/modules/iam/output.tf
+++ terraform/modules/iam/output.tf
@@ -1,4 +1,5 @@
-output "lambda_role_arn" {
-  description = "The ARN of the Lambda execution role"
-  value       = aws_iam_role.lambda_exec_role.arn
+
+output "lambda_exec_role_arn" {
+  description = "ARN of the Lambda execution role"
+  value       = aws_iam_role.lambda_exec.arn
 }
