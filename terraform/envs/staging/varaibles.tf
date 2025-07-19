37bab5bb3430e9ba61a341a936fa9e4aa64ad611
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 23:20:37 +0530

    variable declered

--- terraform/envs/staging/varaibles.tf
+++ terraform/envs/staging/varaibles.tf
@@ -7,3 +7,12 @@ variable "image_uri" {
   description = "Docker image URI for Lambda"
   type        = string
 }
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+}
