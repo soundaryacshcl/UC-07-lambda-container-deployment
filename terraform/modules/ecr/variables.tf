595718a8e9518401eb2be97fe1983bb74126e85f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 20:12:37 +0530

    Update variables.tf

--- terraform/modules/ecr/variables.tf
+++ terraform/modules/ecr/variables.tf
@@ -1,16 +1,14 @@
 variable "project_name" {
   description = "Name of the ECR repository"
   type        = string
-  default     = "lambda-hello-world"  // Default value for the ECR repository name
 }
 
 variable "environment" {
-  description = "Deployment environment (e.g., dev, staging, prod)"
+  description = "Deployment environment"
   type        = string
 }
 
 variable "region" {
-  description = "AWS region for ECR"
+  description = "AWS Region"
   type        = string
-  default     = "ap-south-1"
 }
