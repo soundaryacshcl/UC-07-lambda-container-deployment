d78e6fca01f859f3f9e91e9ce9b14863dbbe62e1
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 23:47:46 +0530

    added modules

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -17,23 +17,28 @@ module "api_gateway" {
   environment          = var.environment
 }
 
-module "vpc" {
-  source      = "../../../modules/vpc"
+module "ecr" {
+  source      = "../../modules/ecr"
+  project_name = var.project_name
   environment = var.environment
+  region      = var.aws_region
 }
 
 module "iam" {
-  source      = "../../../modules/iam"
+  source      = "../../modules/iam"
   environment = var.environment
 }
-
-module "ecr" {
-  source       = "../../../modules/ecr"
-  project_name = var.project_name
-}
-
 module "monitoring" {
-  source      = "../../../modules/monitoring"
+  source      = "../../modules/monitoring"
+  lambda_name = module.lambda.lambda_function_name  // using lambda module output
   environment = var.environment
 }
 
+module "vpc" {
+  source             = "../../modules/vpc"
+  vpc_cidr           = "10.0.0.0/16"
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  environment        = var.environment
+}
