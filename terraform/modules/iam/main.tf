0d831955d4f99e1b5fe13bd0d2cfe352b2cc0e37
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-20 00:25:53 +0530

    tset

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -1,19 +1,51 @@
-resource "aws_iam_role" "lambda_exec" {
+resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role-${var.environment}"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
       }
-    }]
+    ]
   })
 
   tags = {
     Environment = var.environment
-    Name        = "lambda-execution-role"
   }
+}
+
+resource "aws_iam_role_policy" "lambda_ecr_policy" {
+  name = "lambda-ecr-access"
+  role = aws_iam_role.lambda_exec_role.id
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
