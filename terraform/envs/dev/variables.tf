c03b51fd3c8ce3bcc3502e3b7cc512e236dcb858
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 19:57:57 +0530

    Update variables.tf

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -3,11 +3,14 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+
 variable "project_name" {
   description = "Name of the ECR repository"
   type        = string
 }
+
 variable "region" {
-type     = "string"
-default  = "ap-south-1"
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
 }
