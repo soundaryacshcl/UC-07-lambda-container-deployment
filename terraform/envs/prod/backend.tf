868a0b021bc03c2fed362729ec1658e9c77dcc78
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 19:50:11 +0530

    Update backend.tf

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-hcl-usecases"
     key     = "UC-07/envs/prod/terraform.tfstate"
-    region  = "us-east-1"
+    region  = "ap-south-1"
     encrypt = true
   }
 }
