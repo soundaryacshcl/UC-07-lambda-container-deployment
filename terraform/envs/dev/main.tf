33db624168ad5509c7829d92b6385896861fbf73
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 20:56:12 +0530

    vpc region and yml file updates

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -32,6 +32,7 @@ module "vpc" {
   vpc_cidr           = "10.0.0.0/16"
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets    = ["10.0.3.0/24", "10.0.4.0/24"]
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
   environment        = var.environment
 }
+
