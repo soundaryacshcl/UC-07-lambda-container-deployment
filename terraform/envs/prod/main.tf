768b7863759a0a5e6dd839a34e1a79a04069569e
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-19 19:52:44 +0530

    Update main.tf

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -2,7 +2,7 @@ module "ecr" {
   source      = "../../modules/ecr"
   project_name = var.project_name
   environment = var.environment
-  region      = "us-east-1"
+  region      = "ap-south-1"
 }
 
 module "iam" {
